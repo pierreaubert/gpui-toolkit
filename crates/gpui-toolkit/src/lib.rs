acd540593c75041545d14b267dd88cde18adafe9
pierre aubert <pierre@spinorama.org>
2026-06-23 09:48:23 +0200

    added a new crate to re-export everything in one line in Cargo.toml

---
+++ crates/gpui-toolkit/src/lib.rs
@@ -0,0 +1,29 @@
+//! # GPUI Toolkit
+//!
+//! Convenience aggregate crate that re-exports all libraries in the
+//! `gpui-toolkit` workspace so that a single dependency gives access to the
+//! whole toolkit.
+//!
+//! ```rust
+//! use gpui_toolkit::{gpui_ui_kit, gpui_design, gpui_d3rs};
+//! ```
+
+pub use gpui_au;
+pub use gpui_audio_kit;
+pub use gpui_builder;
+pub use gpui_component_lab;
+pub extern crate d3rs as gpui_d3rs;
+pub use gpui_design;
+pub use gpui_design_tools;
+#[cfg(feature = "ios")]
+pub use gpui_ios;
+pub use gpui_keybinding;
+pub use gpui_miniapp;
+pub use gpui_pretext;
+pub use gpui_profiler;
+pub use gpui_px;
+pub use gpui_python_runtime;
+pub use gpui_scaffolder;
+pub use gpui_themes;
+pub use gpui_ui_kit;
+pub use gpui_ui_kit_macros;
